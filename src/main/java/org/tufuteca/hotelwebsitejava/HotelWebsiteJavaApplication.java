0c32d6874fd89e5f26bdb12257f07ee961fd0997
Tufuteca <Tufutecalive@gmail.com>
2024-02-13 22:35:56 +0700

    Создан проект

---
+++ src/main/java/org/tufuteca/hotelwebsitejava/HotelWebsiteJavaApplication.java
@@ -0,0 +1,13 @@
+package org.tufuteca.hotelwebsitejava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelWebsiteJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HotelWebsiteJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/tufuteca/hotelwebsitejava/HotelWebsiteJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.tufuteca.hotelwebsitejava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelWebsiteJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
